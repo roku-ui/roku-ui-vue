401d8cc679d5b969d82f85129ebbda9850674617
Jianqi Pan <jannchie@gmail.com>
2023-12-28 22:19:59 +0900

    :sparkles: feat(image): implement image component

---
+++ packages/vue/src/components/Image.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import { useRounded } from '../utils/classGenerator'
+
+const props = withDefaults(defineProps<{
+  is?: string
+  src?: string
+  size?: 'sm' | 'md' | 'lg' | string | number
+  style?: any
+  class?: any
+  rounded?: 'none' | 'sm' | 'md' | 'lg' | 'full' | string | number
+}>(), {
+  class: '',
+  is: 'img',
+  rounded: 'md',
+})
+const rounded = useRounded(props)
+const loaded = ref(false)
+</script>
+
+<template>
+  <component
+    :is="is"
+    :style="[style, rounded.style]"
+    placeholder
+    :src="src"
+    class="object-cover"
+    :class="[{ hidden: !loaded }, props.class, rounded.class]"
+    @load="loaded = true"
+  />
+</template>
