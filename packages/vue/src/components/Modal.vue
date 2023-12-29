93cf4b5efde3db40213ad44fc1638b6af5c6ac97
Jianqi Pan <jannchie@gmail.com>
2023-12-29 13:32:38 +0900

    :sparkles: feat(modal): implement modal component

---
+++ packages/vue/src/components/Modal.vue
@@ -0,0 +1,83 @@
+<script setup lang="ts">
+import { computed } from 'vue'
+
+const props = withDefaults(defineProps<{
+  modelValue: boolean
+  persistent?: boolean
+  blur?: 'sm' | 'md' | 'lg' | boolean
+}>(), {
+  modelValue: false,
+  persistent: false,
+})
+
+const emit = defineEmits(['update:modelValue'])
+
+const value = computed({
+  get() {
+    return props.modelValue
+  },
+  set(value) {
+    emit('update:modelValue', value)
+  },
+})
+
+const wrapperRef = ref(null)
+function onClick(event: any) {
+  if (!props.persistent) {
+    if (event.target === wrapperRef.value) {
+      value.value = false
+    }
+  }
+}
+
+const blurCls = computed(() => {
+  switch (props.blur) {
+    case 'sm':
+      return 'backdrop-blur-sm'
+    case 'md':
+    case true:
+      return 'backdrop-blur-md'
+    case 'lg':
+      return 'backdrop-blur-lg'
+    default:
+      return ''
+  }
+})
+watchEffect(() => {
+  if (props.modelValue) {
+    document.body.style.overflow = 'hidden'
+    document.body.style.marginRight = '15px'
+  }
+  else {
+    setTimeout(() => {
+      document.body.style.overflow = ''
+      document.body.style.marginRight = ''
+    }, 300)
+  }
+})
+</script>
+
+<template>
+  <Teleport to="body">
+    <div
+      ref="wrapperRef"
+      class="fixed left-0 top-0 z-100 h-full w-full flex items-end justify-center bg-surface-10/50 transition-all duration-300 md:items-center"
+      :class="[blurCls, {
+        ['opacity-0 pointer-events-none']: !value,
+        ['opacity-100']: value,
+      }]"
+      @click="onClick"
+    >
+      <Transition
+        enter-from-class="translate-y-1/2"
+        enter-to-class="translate-y-0"
+        leave-from-class="translate-y-0"
+        leave-to-class="translate-y-1/2"
+        enter-active-class="transition ease-out duration-300"
+        leave-active-class="transition ease-in duration-200"
+      >
+        <slot v-if="value" />
+      </Transition>
+    </div>
+  </Teleport>
+</template>
---
+++ packages/vue/src/components/Paper.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="container-default rounded p-2">
+    <slot />
+  </div>
+</template>
