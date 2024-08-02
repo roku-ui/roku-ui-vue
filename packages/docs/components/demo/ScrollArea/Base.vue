0b6126818c71100e93ff491e9a40d97794cf3310
Jianqi Pan <jannchie@gmail.com>
2024-08-02 20:18:15 +0900

    docs(demo): add scroll area demo

---
+++ packages/docs/components/demo/ScrollArea/Base.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import { ScrollArea } from '@roku-ui/vue'
+
+const code = computed(() => `<template>
+    <ScrollArea>
+        <div
+            v-for="i in 100"
+            :key="i"
+        >
+            {{ i }}
+        </div>
+    </ScrollArea>
+</template>`)
+</script>
+
+<template>
+  <Demo :code="code">
+    <template #preview>
+      <div class="relative h-full w-full p-2">
+        <ScrollArea>
+          <div
+            v-for="i in 100"
+            :key="i"
+          >
+            {{ i }}
+          </div>
+        </ScrollArea>
+      </div>
+    </template>
+  </Demo>
+</template>
