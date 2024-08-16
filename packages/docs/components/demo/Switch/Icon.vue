fb5716f3659f144f2161eb4bdd2eab8dee68ebfe
Jianqi Pan <jannchie@gmail.com>
2024-08-16 22:15:36 +0900

    docs(demo): add switch icon demo

---
+++ packages/docs/components/demo/Switch/Icon.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { Switch } from '@roku-ui/vue'
+
+const code = computed(() => `<script setup>
+const value = ref(false)
+<\/script> 
+<template>
+  <Switch 
+    v-model="value" 
+    off-icon="i-line-md-close" 
+    on-icon="i-line-md-circle"
+  /> 
+</template>`)
+const value = ref(false)
+</script>
+
+<template>
+  <Demo :code="code">
+    <template #preview>
+      <Switch
+        v-model="value"
+        off-icon="i-line-md-close"
+        on-icon="i-line-md-circle"
+      />
+    </template>
+  </Demo>
+</template>
