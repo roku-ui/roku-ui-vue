7a9f4ac635c00ff92ad00490337e6dfa9b153bf9
Jannchie <panjianqi@preferred.jp>
2025-01-29 22:02:41 +0800

    feat(component): auto height transition

---
+++ packages/vue/src/components/AutoHeightTransition.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+function onBeforeEnter(el: Element) {
+  if (el instanceof HTMLElement) {
+    el.style.height = `${0}px`
+  }
+}
+function onEnter(el: Element) {
+  if (el instanceof HTMLElement) {
+    el.style.height = `${el.scrollHeight}px`
+  }
+}
+function onAfterEnter(el: Element) {
+  if (el instanceof HTMLElement) {
+    el.style.height = 'auto'
+  }
+}
+function onBeforeLeave(el: Element) {
+  if (el instanceof HTMLElement) {
+    el.style.height = `${el.scrollHeight}px`
+  }
+}
+function onLeave(el: Element) {
+  if (el instanceof HTMLElement) {
+    el.style.height = `${0}px`
+  }
+}
+</script>
+
+<template>
+  <Transition
+    css
+    mode="out-in"
+    class="transition-height"
+    @before-enter="onBeforeEnter"
+    @enter="onEnter"
+    @after-enter="onAfterEnter"
+    @before-leave="onBeforeLeave"
+    @leave="onLeave"
+  >
+    <slot />
+  </Transition>
+</template>
