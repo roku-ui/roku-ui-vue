64315068fa01cc7881cf55da93da9cf143bef82b
Jianqi Pan <jannchie@gmail.com>
2025-02-16 13:10:47 +0900

    feat(vue/src): add export for types && format import section

--- packages/vue/src/index.ts
+++ packages/vue/src/index.ts
@@ -4,5 +4,7 @@ import 'virtual:uno.css'
 export * from './components'
 export * from './composables'
 export * from './shared'
+export * from './types'
 export * from './utils'
+
 export { VirtualWaterfall, Waterfall } from 'vue-wf'
